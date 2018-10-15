2143ec3be9fb0f9eed333b286261b18267c61094
Christine Stawitz <cstawitz@uw.edu>
2018-10-15 14:35:21 -0700

    Add name mapping function

---
+++ R/name_map.R
@@ -0,0 +1,16 @@
+name_map <-function(toMAS = TRUE, in_name){
+  if(toMAS){
+    new_name <- switch(in_name, "L_at_Amin_Fem_GP_1" = "lmin",
+           "L_at_Amax_Fem_GP_1" = "lmax",
+           "L_at_Amin__GP_1" = "lmin",
+           "Wtlen_1_Fem" = "alpha_f",
+           "Wtlen_2_Fem" = "beta_f",
+           "NatM_p_1_Fem_GP_1" = ,
+           "SR_LN(R0)" = "R0",
+           "SR_BH_steep" = "h",
+           "SR_sigmaR" = "sigma_r",
+           "SR_autocorr" = "rho"
+           )
+  }
+  return(new_name)
+}
