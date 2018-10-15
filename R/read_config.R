0a46848e5f18c18f03e20bbb1150fb5de24584ec
Christine Stawitz <cstawitz@uw.edu>
2018-10-15 10:40:39 -0700

    Update to work with jsonlite customization

--- R/read_config.R
+++ R/read_config.R
@@ -2,5 +2,5 @@
 
 read_config <- function(config_file_path) {
   #Read in config.json file
-  jsonlite::read_json(config_file_path, simplifyVector = TRUE)
+  jsonlite::read_json(config_file_path)
 }
--- R/write_config.R
+++ R/write_config.R
@@ -3,5 +3,5 @@
 write_config <- function(config_list, outfile_path) {
   #Write out config .json file
   jsonlite::write_json(config_list, outfile_path, pretty=TRUE,
-                       auto_unbox=TRUE)
+                       auto_unbox=TRUE, digits = 4, dup_names=TRUE)
 }
