2c549916dada2e279028a65b354432d5957e6813
Christine Stawitz <cstawitz@uw.edu>
2018-10-15 12:37:13 -0700

    Add same options to the write_data file

--- R/write_data.R
+++ R/write_data.R
@@ -2,5 +2,6 @@
 
 write_data <- function(data_list, outfile_path) {
   #Write out data .json file
-  jsonlite::write_json(data_list, outfile_path)
+  jsonlite::write_json(data_list, outfile_path, pretty=TRUE,
+                       auto_unbox=TRUE, digits = 6, dup_names=TRUE)
 }
