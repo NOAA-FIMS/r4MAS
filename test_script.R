3a7a2eb375982bc37113eeb42d793ea6b782c870
Christine Stawitz <cstawitz@uw.edu>
2018-10-18 16:12:21 -0700

    add more test code

--- test_script.R
+++ test_script.R
@@ -1,6 +1,8 @@
 devtools::load_all("C:\\Users\\chris\\Documents\\GitHub\\jsonlite")
 
 case_1_config <- read_config("C:\\Users\\chris\\Documents\\GitHub\\MAS\\Tests\\CaseStudy1\\mas_case_1_config.json")
+case_2_config <- read_config("C:\\Users\\chris\\Documents\\GitHub\\MAS\\Tests\\CaseStudy2\\mas_case_2_config.json")
+
 
 case_1_data <- read_data("C:\\Users\\chris\\Documents\\GitHub\\MAS\\Tests\\CaseStudy1\\mas_case_1_data.json")
 
@@ -11,8 +13,21 @@ write_config(case_1_config, "C:\\Users\\chris\\Documents\\GitHub\\MAS\\Tests\\Ca
 hakepars<- r4ss::SS_parlines("C:\\Users\\chris\\Documents\\StockAssessment\\hake_noageerr\\2018hake_control.ss")
 lapply(hakepars$Label, name_map, toMAS=TRUE)
 growth_ind<- c(2,3,4,7,8)
-sub <- hakepars[growth_ind,]
-create_par_section("growth", 1, "von_bertalanffy_modified",
-                   par_names = sub$Label, par_lo=sub$LO,
-                   par_hi=sub$HI, par_units=rep("cm",5),
-                   par_phase=sub$PHASE, par_value = sub$INIT)
+growth_sub <- hakepars[growth_ind,]
+rec_ind<- c(18,19,20)
+rec_sub <- hakepars[rec_ind,]
+growth <- create_par_section("growth", 1, "von_bertalanffy_modified",
+                   par_names = growth_sub$Label, growth_sub=sub$LO,
+                   par_hi=growth_sub$HI, par_units=c(rep("cm",2),rep(NA,3)),
+                   par_phase=growth_sub$PHASE, par_value = growth_sub$INIT)
+
+rec <- create_par_section("recruitment", 1, "beverton_holt_alt",
+                           par_names = rec_sub$Label, par_lo=rec_sub$LO,
+                           par_hi=rec_sub$HI, par_units=rep(NA,3),
+                           par_phase=rec_sub$PHASE, par_value = rec_sub$INIT)
+M_sub <- hakepars[1,]
+M <- create_par_section("natural_mortality", 1, "", par_names=M_sub$Label, par_lo=M_sub$LO, par_hi=M_sub$HI, par_units=NA, par_phase=M_sub$PHASE, par_value=M_sub$INIT
+)
+
+movement_matrix(1,1,1)
+
