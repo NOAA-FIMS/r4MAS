6c3349b2d94643623a3b110bc99cbb4a4912b6d0
Christine Stawitz - NOAA <47904621+ChristineStawitz-NOAA@users.noreply.github.com>
2019-06-21 15:40:27 -0700

    add roxygen-like comments

--- R/assign_values.R
+++ R/assign_values.R
@@ -1,4 +1,14 @@
+#' Assign environment variables to a list
+#' 
+#' A function that may be called by `lapply()` to assign all of the variables in the given environment to slots within a list. This
+#'
+#' @param X - the name of the variable in `function_env` to assign to the list slot named `X`
+#' @param function_env - the environment in which you want the function to search for the variable named `X`
+#' @param return_list - the list item you want to add the value of `X` to in the slot named `X`
+#' @seealso [base::get()]
+#' @export
+#' @md
 assign_values <- function(X, function_env, return_list){
-  return_list$X <- get(X,                                                function_env)
+  return_list$X <- get(X, function_env)
   return(return_list)
 }
