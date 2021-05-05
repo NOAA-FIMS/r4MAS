81d026dfc6e4d780599c8e108d3ae4303c3bea59
msupernaw <matthew.supernaw@noaa.gov>
2021-05-05 16:36:12 -0400

    Update R4MAS.hpp

--- src/R4MAS.hpp
+++ src/R4MAS.hpp
@@ -6710,7 +6710,6 @@ public:
 
         mas->mas_instance.info.nyears = this->nyears;
         mas->mas_instance.info.nseasons = this->nseasons;
-        std::cout << "integer check: " << mas->mas_instance.info.nyears << "  " << mas->mas_instance.info.nseasons << "\n";
         mas->mas_instance.info.spawning_season_offset = this->spawning_season_offset;
         mas->mas_instance.info.survey_fraction_of_year = this->survey_season_offset;
         mas->mas_instance.info.catch_fraction_of_year = this->catch_season_offset;
