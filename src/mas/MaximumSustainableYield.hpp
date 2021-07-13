adc2a6638212cfbd6d5c6a702935f63a2384039e
Matthew-Supernaw-NOAA <matthew.supernaw@noaa.gov>
2021-07-13 15:51:00 -0400

    initialize msy values to 0

--- src/mas/MaximumSustainableYield.hpp
+++ src/mas/MaximumSustainableYield.hpp
@@ -19,48 +19,48 @@ namespace mas {
 
     template<typename REAL_T>
     struct MaximumSustainableYield {
-        REAL_T spr_F0;
-        REAL_T Fmax;
-        REAL_T msy;
-        REAL_T F_msy;
-        REAL_T spr_msy_out; // = spr[max_index];
-        REAL_T spr_msy;
-        REAL_T SR_msy; // = spr_msy/spr_F0;
-        REAL_T D_msy;
-        REAL_T R_msy;
-        REAL_T SSB_msy;
-        REAL_T B_msy;
-        REAL_T E_msy;
+        REAL_T spr_F0=0;
+        REAL_T Fmax=0;
+        REAL_T msy=0;
+        REAL_T F_msy=0;
+        REAL_T spr_msy_out=0; // = spr[max_index]=0;
+        REAL_T spr_msy=0;
+        REAL_T SR_msy=0; // = spr_msy/spr_F0=0;
+        REAL_T D_msy=0;
+        REAL_T R_msy=0;
+        REAL_T SSB_msy=0;
+        REAL_T B_msy=0;
+        REAL_T E_msy=0;
 
         //F30
-        REAL_T F30;
-        REAL_T spr_F30_msy;
-        REAL_T SR_F30_msy;
-        REAL_T D_F30_msy;
-        REAL_T R_F30_msy;
-        REAL_T SSB_F30_msy;
-        REAL_T B_F30_msy;
-        REAL_T E_F30_msy;
+        REAL_T F30=0;
+        REAL_T spr_F30_msy=0;
+        REAL_T SR_F30_msy=0;
+        REAL_T D_F30_msy=0;
+        REAL_T R_F30_msy=0;
+        REAL_T SSB_F30_msy=0;
+        REAL_T B_F30_msy=0;
+        REAL_T E_F30_msy=0;
 
         //F35
-        REAL_T F35;
-        REAL_T spr_F35_msy;
-        REAL_T SR_F35_msy;
-        REAL_T D_F35_msy;
-        REAL_T R_F35_msy;
-        REAL_T SSB_F35_msy;
-        REAL_T B_F35_msy;
-        REAL_T E_F35_msy;
+        REAL_T F35=0;
+        REAL_T spr_F35_msy=0;
+        REAL_T SR_F35_msy=0;
+        REAL_T D_F35_msy=0;
+        REAL_T R_F35_msy=0;
+        REAL_T SSB_F35_msy=0;
+        REAL_T B_F35_msy=0;
+        REAL_T E_F35_msy=0;
 
         //F40
-        REAL_T F40;
-        REAL_T spr_F40_msy;
-        REAL_T SR_F40_msy;
-        REAL_T D_F40_msy;
-        REAL_T R_F40_msy;
-        REAL_T SSB_F40_msy;
-        REAL_T B_F40_msy;
-        REAL_T E_F40_msy;
+        REAL_T F40=0;
+        REAL_T spr_F40_msy=0;
+        REAL_T SR_F40_msy=0;
+        REAL_T D_F40_msy=0;
+        REAL_T R_F40_msy=0;
+        REAL_T SSB_F40_msy=0;
+        REAL_T B_F40_msy=0;
+        REAL_T E_F40_msy=0;
     };
 
 }
