031613664859a326cb6c2d2db704ad6bb22b7685
Unknown <unknown@example.com>
2021-08-23 12:03:10 -0400

    print just cov

--- src/mas/third_party/ATL/lib/Optimization.hpp
+++ src/mas/third_party/ATL/lib/Optimization.hpp
@@ -837,9 +837,9 @@ namespace atl {
 			g_d(0,i) = atl::Variable<T>::tape.Value(parameters[i]);
 		}
 
-                std::cout<<"\n"<<g<<"\n\n";
+//                std::cout<<"\n"<<g<<"\n\n";
                 std::cout<<cov<<"\n\n";
-                std::cout<<g_d<<"\n\n";
+//                std::cout<<g_d<<"\n\n";
                 
 		atl::RealMatrix<T> ret = g_d*cov*g;
 
