03c62f03a4611938084478b9c690a30d3b3296aa
msupernaw <matthew.supernaw@noaa.gov>
2021-12-06 10:06:56 -0500

    Update Recruitment.hpp

--- src/mas/Recruitment.hpp
+++ src/mas/Recruitment.hpp
@@ -369,7 +369,7 @@ namespace mas {
         variable spawning_biomass_per_recruit_unfished,
                 variable spawning_biomass_per_recruit_f) {
 //=R0*((4*h*R52-(1-h)*$B$7)/((5*h-1)*R52))
-            return (mas::exp(this->log_R0)*(.9))*((4.0*this->h*
+            return mas::exp(this->log_R0)*((4.0*this->h*
                     spawning_biomass_per_recruit_f-(1.0-this->h)*
                     spawning_biomass_per_recruit_unfished)/((5.0*this->h-1.0)
                     *spawning_biomass_per_recruit_f));
