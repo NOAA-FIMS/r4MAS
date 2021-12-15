84082119971d5e6c75d168b76f640a626a1b4299
msupernaw <matthew.supernaw@noaa.gov>
2021-12-15 10:21:57 -0500

    Update Population.hpp

--- src/mas/Population.hpp
+++ src/mas/Population.hpp
@@ -1837,13 +1837,15 @@ namespace mas {
                 B_eq[i] = F_B_eq;
                 L_eq[i] = F_L_eq;
                 E_eq[i] = F_L_sum / F_num_sum;
+                
                 if (L_eq[i] < 0.0) {
 
                     for (int i = 0; i < F.size(); i++) {
                         for (int a = 0; a < ages.size(); a++) {
                             msy_debug <<"a = "<<a<<"\n";
+                            msy_debug<<"L_eq = "<<L_eq[i]<<"\n";
                             msy_debug <<"F_sbpr_unfished = "<<F_sbpr_unfished<<" F_sbpr = "<< F_sbpr<<"\n";
-                            msy_debug << "f = " << F[i] << "  F_L_eq = "<<F_L_eq<<" R_eq = " << R_eq[i] << " spawners_per_recruit = " << spawners_per_recruit[a]<< " equilibrium_landing_numbers = "<<equilibrium_landing_numbers[a] << "\n";
+                            msy_debug << "f = " << F[i] << "  F_L_eq = "<<F_L_eq<<" R_eq = " << R_eq[i] << " spawners_per_recruit = " << spawners_per_recruit[a]<< " equilibrium_numbers = "<<equilibrium_numbers[a] << "\n";
                         }
                     }
                 }
