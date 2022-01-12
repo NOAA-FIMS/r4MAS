ccc09374fd3e9ba75e55ec3f32d2bbe1df887fe1
msupernaw <matthew.supernaw@noaa.gov>
2022-01-12 11:52:07 -0500

    Update Population.hpp

--- src/mas/Population.hpp
+++ src/mas/Population.hpp
@@ -1370,7 +1370,7 @@ namespace mas {
             } else {
                 
                 if (season == this->spawning_season) {
-                    std::cout<<"spawning season..."<<this->recruitment.size()<<" --- "<<(season*year)<<"\n";
+                    std::cout<<"spawning season..."<<this->recruitment_model->recruitment_deviations.size()<<" --- "<<(season*year)<<"\n";
                     //previous year spawning biomass
                     variable sb = this->spawning_stock_biomass[(year - 1) * seasons
                             + (season - 1)];
